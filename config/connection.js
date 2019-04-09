8fd74d04ad2dab88ec5f84b5ca11ffa52d9af98a
Maddie P <madelineprusak@gmail.com>
2019-04-09 16:31:12 -0400

    testing heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection;
+
+
+if (process.env.JAWSDB_URL) {
+ //DB is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+}
+else {
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -8,7 +17,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "safariAnimals_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
